b53e3aab8eb20ea30d04d628a0cc193ad735d0f5
jiayi <jiayizhan2005@gmail.com>
2024-01-29 18:18:12 +0100

    ☠

--- gioco.php
+++ gioco.php
@@ -1,3 +1,8 @@
+<?php
+// $betSuit = $_POST['seme'];
+$betSuit = "c";
+?>
+
 <!DOCTYPE html>
 <html lang="it">
 
@@ -7,25 +12,170 @@
     <title>Inizia la corsa</title>
     <link rel="stylesheet" href="gioco.css">
     <script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
+
+    <style>
+        * {
+            margin: 0;
+            padding: 0;
+            box-sizing: border-box;
+        }
+
+        html,
+        body {
+            height: 100%;
+            width: 100%;
+        }
+
+        .gioco {
+            margin-top: 150px;
+        }
+
+        .field img {
+            display: block;
+        }
+
+        .start {
+            display: flex;
+        }
+
+        .start .deck {
+            width: 300px;
+            padding: 0 50px;
+        }
+
+        .start .cards img {
+            position: relative;
+            margin: 0 12px;
+        }
+
+        #pesca-carta {
+            cursor: pointer;
+        }
+    </style>
 </head>
 
 <body>
     <main class="gioco">
-        <button>
-            click me
-        </button>
+        <div class="field">
+            <img src="./Immagini Carte/dorso.JPG" alt="">
+            <img src="./Immagini Carte/dorso.JPG" alt="">
+            <img src="./Immagini Carte/dorso.JPG" alt="">
+            <img src="./Immagini Carte/dorso.JPG" alt="">
+            <img src="./Immagini Carte/dorso.JPG" alt="">
+        </div>
+
+        <div class="start">
+            <div class="deck">
+                <img src="./Immagini Carte/dorso.JPG" alt="" id="pesca-carta">
+                <img class="card" src="" alt="<-- clicca qui">
+            </div>
+
+            <div class="cards">
+                <img src="./Immagini Carte/bg_c13.gif" alt="" data-suit="c">
+                <img src="./Immagini Carte/bg_d13.gif" alt="" data-suit="d">
+                <img src="./Immagini Carte/bg_h13.gif" alt="" data-suit="h">
+                <img src="./Immagini Carte/bg_s13.gif" alt="" data-suit="s">
+            </div>
+        </div>
     </main>
 
     <script>
+        const START_POINTS = {
+            c: 0,
+            d: 0,
+            h: 0,
+            s: 0,
+        }
+
         $(document).ready(function() {
-            $('button').click(function() {
+            let points = {
+                ...START_POINTS
+            }
+
+            function restartGame() {
+                $(`[data-suit]`).css('top', '0')
+                $(".deck .card").attr('src', '')
+                $('.field img').attr('src', './Immagini Carte/dorso.JPG')
+                points = {
+                    ...START_POINTS
+                }
                 $.ajax({
                     url: 'pesca-carta.php',
                     type: 'GET',
-                    data: {},
+                    cache: false,
+                    data: {
+                        restart: 1
+                    },
+                })
+            }
+
+            function checkGame(suit) {
+                if (points[suit] === 6) {
+                    setTimeout(() => {
+                        const bet = '<?= $betSuit ?>'
+                        const SUITS = {
+                            c: 'fiori',
+                            h: 'cuori',
+                            s: 'picche',
+                            d: 'quadri'
+                        }
+
+                        let message = ''
+                        if (bet == suit) message = 'Complimenti, ha vinto il seme: ' + SUITS[suit]
+                        else message = 'Hai perso, ha vinto il seme: ' + SUITS[suit]
+                        message += "\nVuoi cambiare il seme scommesso?"
+
+                        if (confirm(message)) window.location.replace("/")
+                        else restartGame()
+
+                    }, 200)
+                    return
+                }
+
+                let showCard = points[suit] != 1
+                for (const point of Object.values(points)) showCard &= point >= points[suit]
+                if (!showCard) return
+
+                $.ajax({
+                    url: 'pesca-carta.php',
+                    type: 'GET',
+                    data: {
+                        restart: 0
+                    },
+                    cache: false,
+
+                    success: function(res) {
+                        const cards = $('.field img')
+                        cards.get(cards.length - points[suit] + 1).src = './Immagini Carte/' + res
+
+                        setTimeout(() => {
+                            suit = res[3];
+                            $(`[data-suit="${suit}"]`).css('top', "-=" + $('.field img').height())
+                            points[suit] += 1
+                            checkGame(suit)
+                        }, 200)
+                    }
+                })
+            }
+
+            $('#pesca-carta').click(function() {
+                $.ajax({
+                    url: 'pesca-carta.php',
+                    type: 'GET',
+                    data: {
+                        restart: 0
+                    },
+                    cache: false,
 
                     success: function(response) {
-                        console.log()
+                        $(".deck .card").attr('src', './Immagini Carte/' + response)
+                        const suit = response[3]
+                        points[suit] += 1
+
+                        setTimeout(() => {
+                            $(`[data-suit="${suit}"]`).css('top', "-=" + $('.field img').height())
+                            checkGame(suit)
+                        }, 200)
                     },
                 })
             })
--- pesca-carta.php
+++ pesca-carta.php
@@ -2,21 +2,29 @@
 
 session_start();
 
-if (!isset($_SESSION['cards'])) {
+if ($_GET['restart']) {
   $_SESSION['cards'] = [];
 
   foreach (array('c', 'd', 'h', 's') as $suit)
-    for ($value = 1; $value <= 13; ++$value)
+    for ($value = 1; $value < 13; ++$value)
+      $_SESSION['cards'][] = 'bg_' . $suit . $value . '.gif';
+
+  die();
+}
+
+if (!isset($_SESSION['cards']) || count($_SESSION['cards']) === 0) {
+  $_SESSION['cards'] = [];
+
+  foreach (array('c', 'd', 'h', 's') as $suit)
+    for ($value = 1; $value < 13; ++$value)
       $_SESSION['cards'][] = 'bg_' . $suit . $value . '.gif';
 }
 
 
 $cards = $_SESSION['cards'];
 
-if (count($cards) == 1) {
-  session_destroy();
-}
-
 $key = array_rand($cards);
-echo $cards[$key];
-unset($cards['cards'][$key]);
+$randomCard = $cards[$key];
+unset($_SESSION['cards'][$key]);
+
+echo $randomCard;
